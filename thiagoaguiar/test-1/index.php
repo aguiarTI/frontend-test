89a58ee99504848416da85665d03866de8c042bc
Thiago Aguiar <thiago.aguiar86@gmail.com>
2016-10-26 04:25:02 -0200

    [AMIL] - Desenvolvimento de teste 1 para vaga de front-end.

---
+++ thiagoaguiar/test-1/index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	
+	<title>Amil</title>
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="format-detection" content="telephone=yes"/>
+    <meta name="msapplication-tap-highlight" content="yes"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0"/>
+	<meta name="mobile-web-app-capable" content="yes"/>
+	<meta name="description" content=""/>
+	<meta name="keywords" content=""/>
+	<meta name="googlebot-news" content="noindex, nofollow, nosnippet"/>
+	<meta property="og:title" content=""/>
+	<meta property="og:type" content="article"/>
+	<meta property="og:image" content=""/>
+	<meta property="og:site_name" content="Amil"/>
+	<meta property="og:description" content="" />
+
+	<link rel="manifest" href="package.json"/>
+	<!-- other browsers -->
+	<link rel="shortcut icon" href="img/favicon/amil-16x16.png"/>
+	<!-- IE and safari -->
+	<link rel="icon" href="img/favicon/amil-24x24.png"/>
+	<!-- device others / android -->
+	<link rel="icon" href="img/favicon/amil-64x64.png"/>
+	<!-- apple device's -->
+	<link rel="apple-touch-icon" href="img/favicon/amil-64x64.png"/>
+	<!-- apple ipad -->
+	<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/amil-76x76.png"/>
+	<!-- apple ipad retina -->
+	<link rel="apple-touch-icon" sizes="144x144" href="img/favicon/amil-144x144.png"/>
+	<link rel="apple-touch-icon" sizes="152x152" href="img/favicon/amil-152x152.png"/>
+	<!-- apple iphone -->
+	<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/amil-57x57.png"/>
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/amil-72x72.png"/>
+	<!-- apple iphone retina -->
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/amil-114x114.png"/>
+	<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/amil-120x120.png"/>
+	<!-- google tv -->
+	<link rel="apple-touch-icon" sizes="96x96" href="img/favicon/amil-96x96.png"/>
+
+	<link rel="stylesheet" type="text/css" href="css/common.min.css"/>
+</head>
+<body>
+
+	<header>
+		<nav>
+			<div class="container">
+				<section>
+					<div class="box m-12">
+						<h1>
+							<figure>
+								<a href="#">
+									<img alt="Logo" title="Logo" src="img/logo.svg">
+								</a>
+							</figure>
+						</h1>
+						<ul>
+							<li>
+								<a href="#">Lorem ipsum</a>
+							</li>
+							<li>
+								<a href="#">Dolor</a>
+							</li>
+							<li>
+								<a href="#">Consectetur</a>
+							</li>
+						</ul>
+						<a href="#" class="search"><span>Interdum</span>&#x276f;</a>
+					</div>
+				</section>
+			</div>			
+		</nav>
+		<div class="container">
+			<section>
+				<div class="box m-12">
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Vestibulum<span>&#x276f;</span></a>
+						</li>
+						<li>
+							<a href="#">Ultrices<span>&#x276f;</span></a>
+						</li>
+						<li>
+							<a href="#">Érat egestas<span>&#x276f;</span></a>
+						</li>
+						<li>
+							<a href="#">Varius felis sed</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	</header>
+
+	<div class="container">
+		<!-- gallery -->
+		<section>
+			<div class="box gallery m-9 gray">
+				<div class="swiper-container">
+					<ul class="swiper-wrapper">
+						<li class="swiper-slide orange">
+							<figure>
+								<a href="#">
+									<img alt="Destaque" title="Destaque" src="img/city-700x245.jpg" width="100%" height="100%">
+								</a>
+							</figure>
+						</li><!--
+					 --><li class="swiper-slide purple"></li><!--
+					 --><li class="swiper-slide black"></li>
+					</ul>
+					<div class="navigation"></div>
+				</div>
+			</div><!--
+		 --><div class="box feature m-3">
+		 		<figure>
+					<a href="#">
+			 			<img alt="Fotografia" title="Fotografia" src="http://fpoimg.com/220x244?text=Fotografia" width="100%">
+			 		</a>
+		 		</figure>
+			</div>
+		</section>
+
+		<!-- content -->
+		<section class="content">
+			<div class="box sidebar m-3">
+				<nav>
+					<h3>Áestigulum</h3>
+					<dl>
+						<dt>Vestibulum</dt>
+						<dd><a href="#">Vestibulum ultrices</a></dd>
+						<dd><a href="#">Erat egestas</a></dd>
+						<dd><a href="#">Varius felis sed</a></dd>
+						
+						<dt><a href="#">Áestibuçum</a></dt>
+
+						<dt>Vestibulum</dt>
+						<dd><a href="#">Vestibulum ultrices</a></dd>
+						<dd><a href="#">Erat egestas</a></dd>
+						<dd><a href="#">Varius felis sed</a></dd>
+
+						<dt>Vestibulum</dt>
+						<dd><a href="#">Vestibulum ultrices</a></dd>
+						<dd><a href="#">Erat egestas</a></dd>
+						<dd><a href="#">Varius felis sed</a></dd>
+						
+						<dt><a href="#">Áestibuçum</a></dt>				
+
+						<dt>Vestibulum</dt>
+						<dd><a href="#">Vestibulum ultrices</a></dd>
+						<dd><a href="#">Erat egestas</a></dd>
+						<dd><a href="#">Varius felis sed</a></dd>
+					</dl>
+				</nav>
+			</div><!--
+		 --><div class="box m-6">
+		 		<h3>Etiam sagittis nisi risus</h3>
+		 		<p>Maecenas ut ante posuere, ultrices diam ut, tempus mauris. Nam tincidunt nisl dui, id porta dui ornare vel. Phasellus ac lectus eu metus accumsan elementum fringilla sed felis.</p>
+		 		<p>Nam dui lacus, blandit sit amet aliquam at, lobortis ut mauris. Pellentesque scelerisque velit eu varius ultrices. Curabitur viverra, erat sit amet auctor suscipit, massa lectus dictum erat, imperdiet lobortis mauris diam nec lacus.</p>
+		 	</div><!--
+		 --><div class="box m-3" class="detail">
+		 		<h3>Suspendisse et</h3>
+		 		<figure>
+			 		<a href="#" class="detail">
+			 			<img alt="Fotografia" title="Fotografia" src="http://fpoimg.com/300x250?text=Fotografia" width="100%">
+			 		</a>
+		 		</figure>
+		 	</div><!--
+		 --><div class="box m-9">
+		 		<figure>
+			 		<a href="#">
+			 			<img alt="Fotografia" title="Fotografia" src="http://fpoimg.com/700x200?text=Fotografia" width="100%">
+			 		</a>
+			 		<p>
+			 			<span>Etiam sagittis nisi risus, vitae finibus ligula aliquam sit amet.</span>
+			 		</p>
+		 		</figure>
+		 	</div><!--
+		 --><div class="box option m-3">
+		 		<figure>
+		 			<img src="img/sucesso.svg" alt="" title="">
+		 		</figure>
+		 		<p>Ùonec laoreet sem id vehicula vehicula.</p>
+		 	</div><!--
+		 --><div class="box option m-3">
+		 		<figure>
+		 			<img src="img/erro.svg" alt="" title="">
+		 		</figure>
+		 		<p>Mauris non congue justo, ut convallis purus. Vestibulum ligula.</p>
+		 	</div><!--
+		 --><div class="box option m-3">
+		 		<figure>
+		 			<img src="img/editar.svg" alt="" title="">
+		 		</figure>
+		 		<p>Morbi augue tellus, viverra vestibulum felis et, eleifend consectetur.</p>
+			</div>
+		</section>
+	</div>
+
+	<!-- partners -->
+	<section class="partner">
+		<div class="container">
+			<section>
+				<div class="box m-6">
+			 		<h3>Áuctor commod</h3>
+			 		<nav>
+				 		<h4>Consectetur</h4>
+				 		<ul>
+				 			<li>
+				 				<a href="#">ÁLorem Item 1</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">item 2</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">Lorem 3</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">Domus item 4</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">Domuçs 5</a>
+				 			</li>
+				 		</ul>
+				 		<ul>
+				 			<li>
+				 				<a href="#">Lorem Item 7</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">Item 8</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">Lorem 9</a>
+				 			</li>
+				 		</ul>
+				 		<a href="#" class="more"><span>All things</span> <span>&#x276f;</span></a>
+			 		</nav>
+
+			 		<nav>
+				 		<h4>Vestibulum</h4>
+				 		<ul class="last">
+				 			<li>
+				 				<a href="#">ÁLorem Item 1</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">Item 2</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">Lorem 3</a>
+				 			</li>
+				 			<li>
+				 				<a href="#">Domus item 4</a>
+				 			</li>
+				 		</ul>
+				 		<a href="#" class="more"><span>All things</span> <span>&#x276f;</span></a>
+			 		</nav>
+			 	</div>
+
+			 	<div class="box m-4 brand">
+			 		<h3>Pellentesque</h3>
+			 		<nav>
+			 			<ul class="last">
+			 				<li>
+			 					<figure>
+				 					<a href="#">
+				 						<img src="img/logo-01.png" alt="" title="">
+				 					</a>
+			 					</figure>
+			 				</li>
+			 				<li>
+			 					<figure>
+				 					<a href="#">
+				 						<img src="img/logo-02.png" alt="" title="">
+				 					</a>
+			 					</figure>
+			 				</li>
+			 				<li>
+			 					<figure>
+				 					<a href="#">
+				 						<img src="img/logo-03.png" alt="" title="">
+				 					</a>
+			 					</figure>
+			 				</li>
+			 				<li>
+			 					<figure>
+				 					<a href="#">
+				 						<img src="img/logo-04.png" alt="" title="">
+				 					</a>
+			 					</figure>
+			 				</li>
+			 				<li>
+			 					<figure>
+				 					<a href="#">
+				 						<img src="img/logo-05.png" alt="" title="">
+				 					</a>
+			 					</figure>
+			 				</li>
+			 				<li>
+			 					<figure>
+				 					<a href="#">
+				 						<img src="img/logo-06.png" alt="" title="">
+				 					</a>
+			 					</figure>
+			 				</li>
+			 			</ul>
+			 		</nav>
+			 	</div>
+			</section>
+			<div class="box m-12">
+				<nav class="other">
+					<ul class="first">
+						<li>
+							<a href="#">Lorem ipsum</a>
+						</li>
+						<li>
+							<a href="#">Dolor</a>
+						</li>
+					</ul>
+					<ul class="last">
+						<li>
+							<a href="#">Consectetur</a>
+						</li>
+						<li>
+							<a href="#">Tempus mauris a</a>
+						</li>
+						<li>
+							<a href="#">Phasellus in</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			<p>9999©Ut urna ligula, pretium ut elit dapibus, ornare tempus leo. Suspendisse augue</p>
+			<p>massa Donec in maximus metus. suspendisse volutpat ornare congue.</p>
+		</div>
+	</footer>
+
+	<!-- javascript -->
+	<script type="text/javascript" src="js/common.min.js"></script>
+</body>
+</html>
---
+++ thiagoaguiar/test-1/js/src/app-99-main.js
@@ -0,0 +1,22 @@
+/** ========================
+============================
+@project    : Amil
+@version    : 1.0
+@author     : Thiago Aguiar - thiago.aguiar86@gmail.com
+@copyright  : 2016
+============================
+@begin
+=========================**/
+
+// feature gallery swipe plugin
+var swiper = new Swiper('.swiper-container', {
+      pagination: '.navigation',
+      paginationClickable: true,
+      spaceBetween: 0,
+      loop: true,
+   keyboardControl: true
+});
+
+/** ========================
+end
+=========================**/
